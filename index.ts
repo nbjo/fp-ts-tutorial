5d35b11c60126490f7d3dded642c01acc58fe3b9
nijo dev <nxvdownload@gmail.com>
2024-09-23 00:23:59 +0200

    First commit

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-import './chapter-1.0/2-pipe-into-3-functions';
+// import './chapter-1.0/2-pipe-into-3-functions';
+import { pipe, flow } from "fp-ts/function";
+
+const trim = (s: string) => s.trim();
+
+const size = (s: string) => s.length;
+
+const isAtLeast3 = (n: number) => n >= 3;
+
+const isLongEnough: (s: string) => boolean = flow(trim, size, isAtLeast3); // (s: string) => boolean
+
+// isLongEnough("hello"); // true
+
+const pipeResult: boolean = pipe(
+  " hi42 ",
+  trim, // 'hi'
+  size, // 2
+  isAtLeast3 // false
+); // false
+
+console.log("b = " + pipeResult);
+console.log("isLongEnough(' hi ') = " + isLongEnough(" hi "));
